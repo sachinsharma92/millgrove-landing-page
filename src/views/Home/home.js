e48f01260c5297301a48d591666dc5c7b6205cdb
Abhimanyu Naruka <abhimanyu@grappus.com>
2022-10-28 19:14:53 +0530

    Splash screen added

---
+++ src/views/Home/home.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+// Styles
+import "./home.scss";
+
+// Assets
+import { MILLGROVE_LOGO } from "utils/assets";
+import { MILLGROVE_TREE } from "utils/assets";
+
+function Home(props) {
+  return (
+    <div className="millgrove-home">
+      <span className="millgrove-title">
+        <MILLGROVE_LOGO className="millgrove-logo" />
+      </span>
+      {/* <MILLGROVE_TREE className={"millgrove-tree"} /> */}
+      <div className="millgrove-home-loading">
+        <span>LOADING</span>
+      </div>
+      <div className="millgrove-home-loading-bar">
+        <div className="millgrove-home-loading-bar-fill"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
