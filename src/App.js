4cf67ab6b54e5c085d38ca7bcb0181f7c95eb811
Abhimanyu Naruka <abhimanyu@grappus.com>
2022-11-03 20:59:40 +0530

    Menu screen added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import "./App.scss";
+import { useState } from "react";
+
+// Styles
+import "styles/App.scss";
 
 //Components
 import Home from "./views/Home/home";
+import Menu from "views/Menu/menu";
 
 function App(props) {
+  const [menu, setMenu] = useState(true);
   return (
     <div className="App">
       <Home />
+      {menu && <Menu closeMenu={() => setMenu(false)} />}
     </div>
   );
 }
