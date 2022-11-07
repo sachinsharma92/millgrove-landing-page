2e2da418d83c9493743279d6c320cc26f7a37cd0
DanishOnGit <youcandoit1911@gmail.com>
2022-11-08 00:48:42 +0530

    Add carousel view
    carousel component added. There is a bug in scroll dots

--- src/views/HomeCarousel/HomeCarousel.js
+++ src/views/HomeCarousel/HomeCarousel.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import Header from 'components/Header';
+import React, { useState } from "react";
+import Header from "components/Header";
+import HomeCarouselChild from "./HomeCarouselChild";
+import swimmingPool from "../../assets/images/swimming-pool.png";
+import balcony from "../../assets/images/balcony.png";
+import dining from "../../assets/images/dining.png";
+import bedroom from "../../assets/images/bedroom.png";
 import styles from "./HomeCarousel.module.scss";
+import ScrollIndicator from "./ScrollIndicator";
 
 
 const HomeCarousel = () => {
+  const [activeSlide, setActiveSlide] = useState(0)
+
   return (
     <div className={styles.wrapper}>
-        <Header />
+      <Header />
+      <div className={styles.scrollIndicatorContainer}>
+        <ScrollIndicator activeSlide={activeSlide} nosOfSlides={4} />
+      </div>
+      <div className={styles.carouselWrapper}>
+        <HomeCarouselChild setActiveSlide={setActiveSlide} keyNos={0} houseImage={swimmingPool} description={{ heading: "HOMES", description: "Millgrove homes embrace grand proportions. They are flooded with natural light and designed with sophisticated elegance and intricate craftsmanship. With an abundance of flexible space within each ‘shell & core’ home, you can finish the interior to suit your personal needs and taste. It is a place to be yourself, on a scale like no other." }} />,
+        <HomeCarouselChild setActiveSlide={setActiveSlide} keyNos={1} houseImage={balcony} description={{ heading: "GROUNDS", description: "Cocooned by flourishing greens and blossoming pathways, Millgrove is idyllic. Whether seeking serene time out, reconnection with family and friends, or just a long walk, the sprawling grounds offer an indulgent escape from the bustle of the city. Let nature be your happy place." }} supTextLocation={{ left: "-20%" }} />,
+        <HomeCarouselChild setActiveSlide={setActiveSlide} keyNos={2} houseImage={dining} description={{ heading: "COMFORTS", description: "Millgrove offers a unique opportunity to own a freehold property with the customary comforts found in the best condominiums. Services to each home have been meticulously planned so that you don’t have to worry. It is farmhouse-style living with peace of mind." }} supTextLocation={{ left: "-20%" }} />,
+        <HomeCarouselChild setActiveSlide={setActiveSlide} keyNos={3} houseImage={bedroom} description={{ heading: "LOCATIONS", description: "Millgrove is delicately distanced from the commotion of the city, yet effortlessly linked to the livelier parts of the National Capital Region. On completion of several nearby government infrastructure projects, Millgrove will become one of the most connected places to live in Delhi NCR." }} supTextLocation={{ left: "-20%" }} />
+      </div>
     </div>
   )
 }
---
+++ src/views/HomeCarousel/HomeCarouselChild.js
@@ -0,0 +1,32 @@
+import React, { useRef } from 'react';
+import SuperScriptText from 'components/SuperScriptText';
+import bgImg from "../../assets/images/rectangle-400.png";
+import styles from "./HomeCarousel.module.scss" 
+import { useIntersection } from 'hooks/useIntersection';
+
+const HomeCarouselChild = ({ houseImage, description, bgImage=bgImg,supTextLocation,setActiveSlide,keyNos }) => {
+
+    const slideRef = useRef(null)
+    const inViewport = useIntersection(slideRef, '0px');
+
+    if(inViewport){
+        setActiveSlide(keyNos)
+    }
+
+    return (
+        <div ref={slideRef} className={styles.slide}>
+            <div className={styles.houseAndDescWrapper}>
+            <img src={houseImage} alt={`${houseImage}`} height="40%" width="100%" />
+                <div className={styles.description}>
+                    <SuperScriptText mainText={{text:description.heading,size:"5vw",color:"#2E3430"}} supText={{text:'The',size:"3.5vw",color:"#2E3430",left:supTextLocation?.left}}/>
+                    <p>
+                   {description?.description}
+                    </p>
+                </div>
+            </div>
+            <img className={styles.slideBgImg} src={bgImage} alt="background" height="275" width="100%" />
+        </div>
+    )
+}
+
+export default HomeCarouselChild
