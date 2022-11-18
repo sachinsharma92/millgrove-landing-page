27faa9f4a01a54ddb42b4bfd49b41f6cd3933802
Abhimanyu Naruka <abhimanyu@grappus.com>
2022-11-18 14:51:17 +0530

    [FIX] carousel test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 // Styles
 import "styles/App.scss";
@@ -20,18 +20,85 @@ function App(props) {
   const [menu, setMenu] = useState(false);
   const [loader, setLoader] = useState(false);
   const [cookiesPopup, setCookiesPopup] = useState(false);
-
+  const [progress, setProgress] = useState(0);
+  let container = useRef(null);
   useEffect(() => {
+    handleResize();
     const scroll = new LocomotiveScroll({
-      el: document.querySelector("[data-scroll-container]"),
+      // el: document.querySelector("[data-scroll-container]"),
+      el: container,
       smooth: true,
+      getSpeed: true,
+      getDirection: true,
+      reloadOnContextChange: true,
     });
-    setTimeout(() => {
-      setLoader(false);
-    }, 5000);
+    const fadeInOut = (el1, el2) => {
+      el1.style.animation = "fade-out 0.2s ease-in-out forwards";
+      el2.style.animation = "fade-in 0.2s ease-in-out forwards 0.2s";
+    };
+
+    let scrollDirection = "down";
+    var scrollableElement = document.body; //document.getElementById('scrollableElement');
+
+    scrollableElement.addEventListener("wheel", checkScrollDirection);
+    function checkScrollDirection(event) {
+      if (checkScrollDirectionIsUp(event)) {
+        scrollDirection = "up";
+      } else {
+        scrollDirection = "down";
+      }
+    }
+    function checkScrollDirectionIsUp(event) {
+      if (event.wheelDelta) {
+        return event.wheelDelta > 0;
+      }
+      return event.deltaY < 0;
+    }
+
+    scroll.on("scroll", (args) => {
+      // Get all current elements : args.currentElements
+      if (typeof args.currentElements["carousel-parent-div"] === "object") {
+        let progress = args.currentElements["carousel-parent-div"].progress;
+        const elem1 = document.querySelector(".carousel-div-1");
+        const elem2 = document.querySelector(".carousel-div-2");
+        const elem3 = document.querySelector(".carousel-div-3");
+        const elem4 = document.querySelector(".carousel-div-4");
+        elem1.style.sticky = "sticky !important";
+        elem1.style.top = "0 !important";
+        elem2.style.top = "0 !important";
+        elem3.style.top = "0 !important";
+        elem4.style.top = "0 !important";
+        const scrollDiv = document.querySelector(".home-carousel");
+        let offset =
+          scrollDiv.getBoundingClientRect().bottom / (window.innerHeight * 4);
+        offset = 1 - offset;
+        if (offset > 0.29 && offset < 0.31) {
+          if (scrollDirection === "down") fadeInOut(elem1, elem2);
+          else if (scrollDirection === "up") fadeInOut(elem2, elem1);
+        } else if (offset > 0.49 && offset < 0.51) {
+          if (scrollDirection === "down") fadeInOut(elem2, elem3);
+          else if (scrollDirection === "up") fadeInOut(elem3, elem2);
+        } else if (offset > 0.59 && offset < 0.61) {
+          if (scrollDirection === "down") fadeInOut(elem3, elem4);
+          else if (scrollDirection === "up") fadeInOut(elem4, elem3);
+        }
+        // console.log(args.currentElements["carousel-div"]);
+        // console.log(progress);
+        // ouput log example: 0.34
+        // elem.target.style.opacity = `${progress} !important`;
+        // gsap example : myGsapAnimation.progress(progress);
+      }
+    });
+    // setTimeout(() => {
+    //   setLoader(false);
+    // }, 5000);
   }, []);
   return (
-    <div className="millgrove" data-scroll-container>
+    <div
+      className="millgrove"
+      ref={(el) => (container = el)}
+      data-scroll-container
+    >
       {cookiesPopup && (
         <CookiesPopup closeCookiesPopup={() => setCookiesPopup(false)} />
       )}
@@ -48,3 +115,16 @@ function App(props) {
 }
 
 export default App;
+function handleResize() {
+  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+  const vh = window.innerHeight * 0.01;
+  // Then we set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+  // We listen to the resize event
+  window.addEventListener("resize", () => {
+    // We execute the same script as before
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  });
+}
--- src/views/HomeCarousel/HomeCarousel.js
+++ src/views/HomeCarousel/HomeCarousel.js
@@ -1,86 +1,62 @@
-import React, { useRef, useState } from "react";
-import Header from "components/Header";
-import HomeCarouselChild from "./HomeCarouselChild";
-import swimmingPool from "../../assets/images/swimming-pool.png";
-import balcony from "../../assets/images/balcony.png";
-import dining from "../../assets/images/dining.png";
-import bedroom from "../../assets/images/bedroom.png";
-import styles from "./HomeCarousel.module.scss";
-import ScrollIndicator from "./ScrollIndicator";
-import bgImg from "../../assets/images/rectangle-400.png";
-import { useIntersection } from "../../hooks/useIntersection";
+import React, { useEffect } from "react";
+import "./homecarousel.scss";
 
-const HomeCarousel = () => {
-  const [activeSlide, setActiveSlide] = useState(0);
-  const carouselViewRef = useRef();
-  // const isVisible=useIntersection(carouselViewRef,`-${Math.round(window.innerHeight/2)}px`)
+function HomeCarousel(props) {
+  // useEffect(() => {
+  //   const target = document.querySelector(".home-carousel");
+  //   function handleIntersection(entries) {
+  //     entries.forEach((entry, observer, observe) => {
+  //       if (entry.isIntersecting) {
+  //         document.addEventListener("scroll", () => {
+  //           console.log(entry.target.getBoundingClientRect().top);
+  //           const offset = window.innerHeight;
+  //           const stickyDiv = document.querySelector(".carousel-div");
+  //         });
+  //       } else {
+  //       }
+  //     });
+  //   }
+  //   const observer = new IntersectionObserver(handleIntersection, {
+  //     threshold: 0.25,
+  //   });
+  //   observer.observe(target);
+  // }, []);
 
-  // if(isVisible){
-  //   console.log('ha aajao',window.innerHeight/2)
-  // }
   return (
-    <div ref={carouselViewRef} className={styles.wrapper} data-scroll-section>
-      <Header />
-      <div className={styles.scrollIndicatorContainer}>
-        <ScrollIndicator activeSlide={activeSlide} nosOfSlides={4} />
-      </div>
-      <img
-        className={styles.slideBgImg}
-        src={bgImg}
-        alt="background"
-        height="275"
-        width="100%"
-      />
-      <div className={styles.carouselWrapper}>
-        <HomeCarouselChild
-          setActiveSlide={setActiveSlide}
-          keyNos={0}
-          houseImage={swimmingPool}
-          description={{
-            heading: "HOMES",
-            description:
-              "Millgrove homes embrace grand proportions. They are flooded with natural light and designed with sophisticated elegance and intricate craftsmanship. With an abundance of flexible space within each ‘shell & core’ home, you can finish the interior to suit your personal needs and taste. It is a place to be yourself, on a scale like no other.",
-          }}
-        />
-        ,
-        <HomeCarouselChild
-          setActiveSlide={setActiveSlide}
-          keyNos={1}
-          houseImage={balcony}
-          description={{
-            heading: "GROUNDS",
-            description:
-              "Cocooned by flourishing greens and blossoming pathways, Millgrove is idyllic. Whether seeking serene time out, reconnection with family and friends, or just a long walk, the sprawling grounds offer an indulgent escape from the bustle of the city. Let nature be your happy place.",
-          }}
-          supTextLocation={{ left: "-20%" }}
-        />
-        ,
-        <HomeCarouselChild
-          setActiveSlide={setActiveSlide}
-          keyNos={2}
-          houseImage={dining}
-          description={{
-            heading: "COMFORTS",
-            description:
-              "Millgrove offers a unique opportunity to own a freehold property with the customary comforts found in the best condominiums. Services to each home have been meticulously planned so that you don’t have to worry. It is farmhouse-style living with peace of mind.",
-          }}
-          supTextLocation={{ left: "-20%" }}
-        />
-        ,
-        <HomeCarouselChild
-          setActiveSlide={setActiveSlide}
-          keyNos={3}
-          houseImage={bedroom}
-          description={{
-            heading: "LOCATIONS",
-            description:
-              "Millgrove is delicately distanced from the commotion of the city, yet effortlessly linked to the livelier parts of the National Capital Region. On completion of several nearby government infrastructure projects, Millgrove will become one of the most connected places to live in Delhi NCR.",
-          }}
-          supTextLocation={{ left: "-20%" }}
-        />
-      </div>
+    <div
+      id="stick-carousel"
+      data-scroll-id="carousel-parent-div"
+      className="home-carousel"
+      data-scroll
+      data-scroll-section
+      data-scroll-position="top"
+    >
+      <div
+        className="carousel-div-1"
+        data-scroll
+        data-scroll-sticky
+        data-scroll-target="#stick-carousel"
+      ></div>
+      <div
+        className="carousel-div-2"
+        data-scroll
+        data-scroll-sticky
+        data-scroll-target="#stick-carousel"
+      ></div>
+      <div
+        className="carousel-div-3"
+        data-scroll
+        data-scroll-sticky
+        data-scroll-target="#stick-carousel"
+      ></div>
+      <div
+        className="carousel-div-4"
+        data-scroll
+        data-scroll-sticky
+        data-scroll-target="#stick-carousel"
+      ></div>
     </div>
   );
-};
+}
 
 export default HomeCarousel;
