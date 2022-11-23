dbc937acf20c270c224d1bd830b8d503f3fe4940
Abhimanyu Naruka <abhimanyu@grappus.com>
2022-11-23 16:11:02 +0530

    [FIX] mobile scroll fix type 3

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import LocomotiveScroll from "locomotive-scroll";
 import { useIntersection } from "hooks/useIntersection";
 import debounce from "utils/debounce";
 import HomeCarousel from "views/HomeCarousel/HomeCarousel";
+import { useMediaQuery } from "hooks/useMediaQuery";
 
 function App(props) {
   const [menu, setMenu] = useState(false);
@@ -32,34 +33,17 @@ function App(props) {
     `0px 0px -${window.innerHeight / 1.1}px 0px`
   );
 
-  const animate = ({ scrollDirection, isCarouselInView }) => {
-    if (!isCarouselInView) return;
-
-    if (scrollDirection === "down") {
-      setActiveSlide((prev) => {
-        if (prev === 3) return 3;
-        return prev + 1;
-      });
-    } else if (scrollDirection === "up") {
-      setActiveSlide((prev) => {
-        if (prev === 0) return 0;
-        return prev - 1;
-      });
-    } else {
-      setActiveSlide((prev) => {
-        if (prev === 3) return 3;
-        return prev + 1;
-      });
-    }
-  };
+  const isMobile = useMediaQuery("(max-width: 768px)");
 
   useEffect(() => {
-    scrollRef.current = new LocomotiveScroll({
-      el: document.querySelector("[data-scroll-container]"),
-      smooth: true,
-      getDirection: true,
-      smoothMobile: true,
-    });
+    if (!isMobile) {
+      scrollRef.current = new LocomotiveScroll({
+        el: document.querySelector("[data-scroll-container]"),
+        smooth: true,
+        getDirection: true,
+        smoothMobile: true,
+      });
+    }
 
     setTimeout(() => {
       setLoader(false);
@@ -67,68 +51,69 @@ function App(props) {
   }, []);
 
   useEffect(() => {
-    scrollRef.current.on("scroll", (instance) => {
-      if (isCarouselInView.current) {
-        const elem1 = document.querySelector(".slide1");
-        const elem1abs = document.querySelector(".slide1-abs");
-        const elem2 = document.querySelector(".slide2");
-        const elem3 = document.querySelector(".slide3");
-        const elem4 = document.querySelector(".slide4");
-        const elem4abs = document.querySelector(".slide4-abs");
+    !isMobile &&
+      scrollRef.current.on("scroll", (instance) => {
+        if (isCarouselInView.current) {
+          const elem1 = document.querySelector(".slide1");
+          const elem1abs = document.querySelector(".slide1-abs");
+          const elem2 = document.querySelector(".slide2");
+          const elem3 = document.querySelector(".slide3");
+          const elem4 = document.querySelector(".slide4");
+          const elem4abs = document.querySelector(".slide4-abs");
 
-        const scroll1 = document.querySelector(".carousel-logic-div-1");
-        const scroll2 = document.querySelector(".carousel-logic-div-2");
-        const scroll3 = document.querySelector(".carousel-logic-div-3");
-        const scroll4 = document.querySelector(".carousel-logic-div-4");
+          const scroll1 = document.querySelector(".carousel-logic-div-1");
+          const scroll2 = document.querySelector(".carousel-logic-div-2");
+          const scroll3 = document.querySelector(".carousel-logic-div-3");
+          const scroll4 = document.querySelector(".carousel-logic-div-4");
 
-        if (
-          document.body.scrollTop > scroll1.getBoundingClientRect().top &&
-          document.body.scrollTop < scroll1.getBoundingClientRect().bottom
-        ) {
-          elem1.classList.remove("invisible");
-          if (instance.direction === "down")
-            elem1.classList.add("visible-noanim");
-          else elem1.classList.add("visible");
-        } else {
-          elem1.classList.remove("visible");
-          elem1.classList.remove("visible-noanim");
-          elem1.classList.add("invisible");
+          if (
+            document.body.scrollTop > scroll1.getBoundingClientRect().top &&
+            document.body.scrollTop < scroll1.getBoundingClientRect().bottom
+          ) {
+            elem1.classList.remove("invisible");
+            if (instance.direction === "down")
+              elem1.classList.add("visible-noanim");
+            else elem1.classList.add("visible");
+          } else {
+            elem1.classList.remove("visible");
+            elem1.classList.remove("visible-noanim");
+            elem1.classList.add("invisible");
+          }
+          if (
+            document.body.scrollTop > scroll2.getBoundingClientRect().top &&
+            document.body.scrollTop < scroll2.getBoundingClientRect().bottom
+          ) {
+            elem2.classList.remove("invisible");
+            elem2.classList.add("visible");
+          } else {
+            elem2.classList.remove("visible");
+            elem2.classList.add("invisible");
+          }
+          if (
+            document.body.scrollTop > scroll3.getBoundingClientRect().top &&
+            document.body.scrollTop < scroll3.getBoundingClientRect().bottom
+          ) {
+            elem3.classList.remove("invisible");
+            elem3.classList.add("visible");
+          } else {
+            elem3.classList.remove("visible");
+            elem3.classList.add("invisible");
+          }
+          if (
+            document.body.scrollTop > scroll4.getBoundingClientRect().top &&
+            document.body.scrollTop < scroll4.getBoundingClientRect().bottom
+          ) {
+            elem4.classList.remove("invisible");
+            if (instance.direction === "up")
+              elem4.classList.add("visible-noanim");
+            else elem4.classList.add("visible");
+          } else {
+            elem4.classList.remove("visible");
+            elem4.classList.remove("visible-noanim");
+            elem4.classList.add("invisible");
+          }
         }
-        if (
-          document.body.scrollTop > scroll2.getBoundingClientRect().top &&
-          document.body.scrollTop < scroll2.getBoundingClientRect().bottom
-        ) {
-          elem2.classList.remove("invisible");
-          elem2.classList.add("visible");
-        } else {
-          elem2.classList.remove("visible");
-          elem2.classList.add("invisible");
-        }
-        if (
-          document.body.scrollTop > scroll3.getBoundingClientRect().top &&
-          document.body.scrollTop < scroll3.getBoundingClientRect().bottom
-        ) {
-          elem3.classList.remove("invisible");
-          elem3.classList.add("visible");
-        } else {
-          elem3.classList.remove("visible");
-          elem3.classList.add("invisible");
-        }
-        if (
-          document.body.scrollTop > scroll4.getBoundingClientRect().top &&
-          document.body.scrollTop < scroll4.getBoundingClientRect().bottom
-        ) {
-          elem4.classList.remove("invisible");
-          if (instance.direction === "up")
-            elem4.classList.add("visible-noanim");
-          else elem4.classList.add("visible");
-        } else {
-          elem4.classList.remove("visible");
-          elem4.classList.remove("visible-noanim");
-          elem4.classList.add("invisible");
-        }
-      }
-    });
+      });
   }, [isCarouselInView.current]);
 
   return (
