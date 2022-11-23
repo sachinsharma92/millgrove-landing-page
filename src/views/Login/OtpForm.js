c29b880a4fa6e85e740dd58fc3d85387d585edf1
DanishOnGit <youcandoit1911@gmail.com>
2022-11-23 19:10:23 +0530

    add:Add phone nos error state

--- src/views/Login/OtpForm.js
+++ src/views/Login/OtpForm.js
@@ -1,51 +1,68 @@
-import Button from 'components/Button'
-import { AuthContext } from 'context/AuthContext'
-import React, { useContext, useEffect, useRef, useState } from 'react'
-import OtpInput from 'react18-input-otp'
-import { MILLGROVE_TREE } from 'utils/assets'
-import styles from "./Login.module.scss"
+import Button from "components/Button";
+import { AuthContext } from "context/AuthContext";
+import React, { useContext, useEffect, useRef, useState } from "react";
+import OtpInput from "react18-input-otp";
+import { MILLGROVE_TREE } from "utils/assets";
+import styles from "./Login.module.scss";
+import Timer from "./Timer";
 
 const OtpForm = ({ setIsLoggingIn }) => {
+  const [otp, setOtp] = useState(null);
+  const otpWrapperRef = useRef();
+  const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
 
-    const [otp, setOtp] = useState(null)
-    const otpWrapperRef = useRef();
-    const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext)
+  const handleSubmit = () => {
+    if (otp !== "111111") return;
+    setIsLoggingIn(false);
+  };
 
-    const handleSubmit = () => {
-        setIsLoggingIn(false)
+  const handleChange = (otp) => {
+    setOtp(otp);
+  };
+
+  useEffect(() => {
+    if (otp === "111111") {
+      setIsLoggedIn(true);
     }
+  }, [otp]);
 
-    const handleChange = (otp) => {
-        setOtp(otp)
-    }
-
-    useEffect(() => {
-        if (otp === "111111") {
-            setIsLoggedIn(true)
-        }
-    }, [otp])
-
-    return (
-        <div className={styles.otpMainWrapper}>
-            <div className={styles.bgTree}>
-                <MILLGROVE_TREE style={{ height: '200' }} fillColor="#8a7f7f14" />
-            </div>
-            <div className={styles.otpFormWrapper}>
-                <div className={styles.headingWrapper}>
-                    <h3 className={styles.heading}>
-                        Enter OTP
-                    </h3>
-                </div>
-                <div ref={otpWrapperRef} className={styles.otpWrapper}>
-                    <OtpInput inputStyle={styles.otpInput} value={otp} shouldAutoFocus onChange={handleChange} numInputs={6} />
-                </div>
-                <p className={styles.resend}><span>Resend Code</span> in 00:06 Secs</p>
-                <div className={styles.submitBtnWrapper}>
-                    <Button clickhandler={handleSubmit} text={"Continue"} customStyles={{ backgroundColor: "#2E3430", color: "#F1EFE5", width: "85%", padding: "1rem", textTransform: "uppercase" }} />
-                </div>
-            </div>
+  return (
+    <div className={styles.otpMainWrapper}>
+      <div className={styles.bgTree}>
+        <MILLGROVE_TREE style={{ height: "200" }} fillColor="#8a7f7f14" />
+      </div>
+      <div className={styles.otpFormWrapper}>
+        <div className={styles.headingWrapper}>
+          <h3 className={styles.heading}>Enter OTP</h3>
         </div>
-    )
-}
+        <div ref={otpWrapperRef} className={styles.otpWrapper}>
+          <OtpInput
+            inputStyle={styles.otpInput}
+            value={otp}
+            shouldAutoFocus
+            onChange={handleChange}
+            numInputs={6}
+          />
+        </div>
+        <p className={styles.resend}>
+          <Timer initialMinutes={0} initialSeconds={60} />
+        </p>
+        <div className={styles.submitBtnWrapper}>
+          <Button
+            clickhandler={handleSubmit}
+            text={"Continue"}
+            customStyles={{
+              backgroundColor: "#2E3430",
+              color: "#F1EFE5",
+              width: "85%",
+              padding: "1rem",
+              textTransform: "uppercase",
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default OtpForm
+export default OtpForm;
--- src/views/Login/PhoneNumberForm.js
+++ src/views/Login/PhoneNumberForm.js
@@ -1,64 +1,97 @@
-import { DownArrow } from 'assets/icons/icons'
-import Button from 'components/Button'
-import Layout from 'components/Layout'
-import React, { useState } from 'react'
-import OtpInput from 'react18-input-otp'
-import { MILLGROVE_TREE } from 'utils/assets'
-import { countries } from './countries'
-import styles from "./Login.module.scss"
+import { DownArrow } from "assets/icons/icons";
+import Button from "components/Button";
+import Layout from "components/Layout";
+import React, { useState } from "react";
+import OtpInput from "react18-input-otp";
+import { MILLGROVE_TREE } from "utils/assets";
+import { countries } from "./countries";
+import styles from "./Login.module.scss";
 
 const PhoneNumberForm = ({ setIsEnteringPhoneNos, setIsEnteringOtp }) => {
-    const [phoneNos, setPhoneNos] = useState(null)
-    const [countryCode, setCountryCode] = useState('+91')
+  const [phoneNos, setPhoneNos] = useState(null);
+  const [countryCode, setCountryCode] = useState("+91");
+  const [error, setError] = useState({ errorOccured: false, msg: "" });
 
-    const continueHandler = () => {
-        setIsEnteringPhoneNos(false)
-        setIsEnteringOtp(true)
+  const continueHandler = () => {
+    if (phoneNos !== "3333333333") {
+      setError((prev) => ({
+        ...prev,
+        errorOccured: true,
+        msg: "Phone number not registered.",
+      }));
+      return;
     }
+    setIsEnteringPhoneNos(false);
+    setIsEnteringOtp(true);
+  };
 
-    const handleChange = (val) => {
-        setPhoneNos(val)
-    }
+  const handleChange = (val) => {
+    setPhoneNos(val);
+    setError({ errorOccured: false, msg: "" });
+  };
 
-    return (
-        <Layout>
-            <div className={styles.mainWrapper}>
-                <div className={styles.bgTree}>
-                    <MILLGROVE_TREE style={{ height: '6vw' }} fillColor="#8a7f7f14" />
-                </div>
-                <div className={styles.formWrapper}>
-                    <div className={styles.headingWrapper}>
-                        <h3 className={styles.heading}>
-                            Enter Phone Number
-                        </h3>
-                    </div>
-                    <div className={styles.phoneNosWrapper}>
-                        <div className={styles.countryCodeWrapper}>
-                            <div className={styles.countryCode}>
-                                <div>
-                                    +
-                                </div>
-                                <select onChange={(e) => { console.log(e.target.value); setCountryCode(`+${e.target.value}`) }}>
-                                    {
-                                        countries.map((country, idx) => <option value={country.dial_code} selected={country.dial_code === "91"}>{country.dial_code}</option>)
-                                    }
-                                </select>
-                            </div>
-                            <div className={styles.downArrowWrapper}>
-                                <DownArrow />
-                            </div>
-                        </div>
-                        {/* using otp comp for phone nos as well */}
-                        <OtpInput containerStyle={styles.phoneNosInputContainer} inputStyle={styles.phoneNos} value={phoneNos} shouldAutoFocus onChange={handleChange} numInputs={10} separator={<span>{" "}</span>} separateAfter={4} />
-                    </div>
-                    <p className={styles.instructionText}>Please press continue to proceed.</p>
-                    <div className={styles.submitBtnWrapper}>
-                        <Button isDisabled={!phoneNos ? true : false} clickhandler={continueHandler} text={"Continue"} />
-                    </div>
-                </div>
+  return (
+    <Layout>
+      <div className={styles.mainWrapper}>
+        <div className={styles.bgTree}>
+          <MILLGROVE_TREE style={{ height: "6vw" }} fillColor="#8a7f7f14" />
+        </div>
+        <div className={styles.formWrapper}>
+          <div className={styles.headingWrapper}>
+            <h3 className={styles.heading}>Enter Phone Number</h3>
+          </div>
+          <div className={styles.phoneNosWrapper}>
+            <div className={styles.countryCodeWrapper}>
+              <div className={styles.countryCode}>
+                <div>+</div>
+                <select onChange={(e) => setCountryCode(`+${e.target.value}`)}>
+                  {countries.map((country, idx) => (
+                    <option
+                      value={country.dial_code}
+                      selected={country.dial_code === "91"}
+                    >
+                      {country.dial_code}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className={styles.downArrowWrapper}>
+                <DownArrow />
+              </div>
             </div>
-        </Layout>
-    )
-}
+            {/* using otp comp for phone nos as well */}
+            <OtpInput
+              containerStyle={styles.phoneNosInputContainer}
+              inputStyle={styles.phoneNos}
+              value={phoneNos}
+              shouldAutoFocus
+              onChange={handleChange}
+              numInputs={10}
+              separator={<span> </span>}
+              separateAfter={4}
+            />
+          </div>
+          <p className={styles.instructionText}>
+            {error.errorOccured
+              ? error.msg
+              : "Please press continue to proceed."}
+          </p>
+          {error.errorOccured && (
+            <p className={styles.instructionText}>
+              Please register <span className={styles.navigate}>here</span>
+            </p>
+          )}
+          <div className={styles.submitBtnWrapper}>
+            <Button
+              isDisabled={!phoneNos ? true : false}
+              clickhandler={continueHandler}
+              text={"Continue"}
+            />
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
 
-export default PhoneNumberForm
+export default PhoneNumberForm;
