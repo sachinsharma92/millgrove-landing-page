fa76e0a59b2fff9a5db76d854e40ff08f66cc21f
DanishOnGit <youcandoit1911@gmail.com>
2022-11-23 21:02:07 +0530

    Add new phone nos component

--- src/views/Login/PhoneNumberForm.js
+++ src/views/Login/PhoneNumberForm.js
@@ -2,18 +2,19 @@ import { DownArrow } from "assets/icons/icons";
 import Button from "components/Button";
 import Layout from "components/Layout";
 import React, { useState } from "react";
+import PhoneInput from "react-phone-number-input";
 import OtpInput from "react18-input-otp";
 import { MILLGROVE_TREE } from "utils/assets";
 import { countries } from "./countries";
 import styles from "./Login.module.scss";
+import "react-phone-number-input/style.css";
 
 const PhoneNumberForm = ({ setIsEnteringPhoneNos, setIsEnteringOtp }) => {
   const [phoneNos, setPhoneNos] = useState(null);
-  const [countryCode, setCountryCode] = useState("+91");
   const [error, setError] = useState({ errorOccured: false, msg: "" });
 
   const continueHandler = () => {
-    if (phoneNos !== "3333333333") {
+    if (phoneNos !== "+913333333333") {
       setError((prev) => ({
         ...prev,
         errorOccured: true,
@@ -41,35 +42,12 @@ const PhoneNumberForm = ({ setIsEnteringPhoneNos, setIsEnteringOtp }) => {
             <h3 className={styles.heading}>Enter Phone Number</h3>
           </div>
           <div className={styles.phoneNosWrapper}>
-            <div className={styles.countryCodeWrapper}>
-              <div className={styles.countryCode}>
-                <div>+</div>
-                <select onChange={(e) => setCountryCode(`+${e.target.value}`)}>
-                  {countries.map((country, idx) => (
-                    <option
-                      value={country.dial_code}
-                      selected={country.dial_code === "91"}
-                    >
-                      {country.dial_code}
-                    </option>
-                  ))}
-                </select>
-              </div>
-              <div className={styles.downArrowWrapper}>
-                <DownArrow />
-              </div>
-            </div>
-            {/* using otp comp for phone nos as well */}
-            <OtpInput
-              containerStyle={styles.phoneNosInputContainer}
-              inputStyle={styles.phoneNos}
+            <PhoneInput
+              international
+              countryCallingCodeEditable={false}
+              defaultCountry="IN"
               value={phoneNos}
-              shouldAutoFocus
               onChange={handleChange}
-              numInputs={10}
-              separator={<span> </span>}
-              separateAfter={4}
-              isInputNum
             />
           </div>
           <p className={styles.instructionText}>
