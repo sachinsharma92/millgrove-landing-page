0cc5a4b12b705ef8f31260f2891f0d492f9a70b7
Abhimanyu Naruka <abhimanyu@grappus.com>
2022-11-24 18:13:04 +0530

    [FIX] login responsive

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,29 +1,45 @@
-import React from 'react';
-import styles from './Button.module.scss'
+import React from "react";
+import styles from "./Button.module.scss";
 
-const Button = ({ classname, leftIcon, rightIcon, text, customStyles, type, isRounded, clickhandler = () => { }, isDisabled }) => {
-
-    const getClassname = (btnType) => {
-        switch (btnType) {
-            case 'primary': {
-                return styles.primary
-            }
-            case 'secondary': {
-                return styles.secondary
-            }
-            default: {
-                return styles.primary
-            }
-        }
+const Button = ({
+  classname,
+  leftIcon,
+  rightIcon,
+  text,
+  customStyles,
+  type,
+  isRounded,
+  clickhandler = () => {},
+  isDisabled,
+}) => {
+  const getClassname = (btnType) => {
+    switch (btnType) {
+      case "primary": {
+        return styles.primary;
+      }
+      case "secondary": {
+        return styles.secondary;
+      }
+      default: {
+        return styles.primary;
+      }
     }
+  };
 
-    return (
-        <button onClick={clickhandler} disabled={isDisabled} className={`${classname} ${getClassname(type)} ${styles.btn} ${isRounded && styles.rounded}`} style={customStyles && { ...customStyles }}>
-            {leftIcon && <span className={styles.btnIcon}>{leftIcon}</span>}
-            {text && <span className={styles.btnText}>{text}</span>}
-            {rightIcon && <span className={styles.btnIcon}>{rightIcon}</span>}
-        </button>
-    )
-}
+  return (
+    <button
+      onClick={clickhandler}
+      disabled={isDisabled}
+      className={`${classname} ${getClassname(type)} ${styles.btn} ${
+        isRounded && styles.rounded
+      }`}
+      style={customStyles && { ...customStyles }}
+    >
+      {leftIcon && <span className={styles.btnIcon}>{leftIcon}</span>}
+      {text && <span className={styles.btnText}>{text}</span>}
+      {rightIcon && <span className={styles.btnIcon}>{rightIcon}</span>}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
