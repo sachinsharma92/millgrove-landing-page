1d27975684b9d05d79508d705f8d9e95cf6e0b3c
sachinsharma92 <sachindesigner2018@gmail.com>
2022-12-03 12:25:20 +0530

    FIX: homepage mobile view

--- src/views/HomeCarouselMobile/HomeCarouselMobile.js
+++ src/views/HomeCarouselMobile/HomeCarouselMobile.js
@@ -1,75 +1,61 @@
-import React, { useState } from "react";
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import { MILLGROVE_LOGO } from "utils/assets";
+
 
 // Styles
 import "./homecarouselmobile.scss";
 
-//Assets
-import carouselImg1 from "assets/images/carousel-img-1.png";
-import carouselImg2 from "assets/images/carousel-img-2.png";
-import carouselImg3 from "assets/images/carousel-img-3.png";
-import carouselImg4 from "assets/images/carousel-img-4.png";
-import { LEFT_ARROW, RIGHT_ARROW } from "utils/assets";
 
-const CAROUSEL_DATA_MOBILE = [
+const sliderData = [
   {
     title: "homes",
     text: "Millgrove homes embrace grand proportions. They are flooded with natural light and designed with sophisticated elegance and intricate craftsmanship. With an abundance of flexible space within each ‘shell & core’ home, you can finish the interior to suit your personal needs and taste. It is a place to be yourself, on a scale like no other.",
-    img: carouselImg1,
+    img: 'https://cdn.pixabay.com/photo/2015/10/20/18/57/furniture-998265_1280.jpg',
   },
   {
     title: "grounds",
     text: "Cocooned by flourishing greens and blossoming pathways, Millgrove is idyllic. Whether seeking serene time out, reconnecting with family and friends, or just taking a long walk, the sprawling grounds offer an indulgent escape from the bustle of the city. Let nature be your happy place.",
-    img: carouselImg2,
+    img: 'https://cdn.pixabay.com/photo/2019/02/03/15/45/winter-3972851_1280.jpg',
   },
   {
     title: "comforts",
     text: "Millgrove offers a unique opportunity to own a freehold property with the customary comforts found in the best condominiums. Services to each home have been meticulously planned so that you don’t have to worry. It is farmhouse-style living with peace of mind.",
-    img: carouselImg3,
+    img: 'https://cdn.pixabay.com/photo/2015/01/29/22/25/old-people-616718_1280.jpg',
   },
   {
     title: "location",
     text: "Millgrove is delicately distanced from the commotion of the city, yet effortlessly linked to thelivelier parts of the National Capital Region. On completion of several nearby government infrastructure projects, Millgrove will become one of the most connected places to live in Delhi NCR.",
-    img: carouselImg4,
+    img: 'https://cdn.pixabay.com/photo/2018/03/25/00/44/panorama-3258403_1280.jpg',
   },
 ];
 
 function HomeCarouselMobile(props) {
-  const [currentItem, setCurrentItem] = useState(0);
   return (
     <div className="home-carousel-mobile">
-      <div className="carousel-text-mobile">
-        <div className="carousel-heading-mobile">
-          {CAROUSEL_DATA_MOBILE[currentItem].title}
-        </div>
-        <div className="carousel-subtext-mobile">
-          {CAROUSEL_DATA_MOBILE[currentItem].text}
-        </div>
-        <div className="carousel-btns-mobile">
-          <div
-            style={{ opacity: currentItem > 0 ? 1 : 0.3 }}
-            onClick={() => currentItem > 0 && setCurrentItem(currentItem - 1)}
-          >
-            <LEFT_ARROW />
-          </div>
-          <div
-            style={{
-              opacity: currentItem < CAROUSEL_DATA_MOBILE.length - 1 ? 1 : 0.3,
-            }}
-            onClick={() =>
-              currentItem < CAROUSEL_DATA_MOBILE.length - 1 &&
-              setCurrentItem(currentItem + 1)
-            }
-          >
-            <RIGHT_ARROW />
-          </div>
-        </div>
+      <div className="mg-first-fold-header" >
+        <MILLGROVE_LOGO style={{ color: '#F4F1EC' }} />
       </div>
-      <div
-        className="carousel-img-mobile"
-        style={{
-          backgroundImage: `url(${CAROUSEL_DATA_MOBILE[currentItem].img})`,
-        }}
-      />
+
+      <Carousel infiniteLoop={true} showThumbs={false} showArrows={false} showStatus={false}>
+        {sliderData.map((item, index) => (
+          <div className="slider-item" key={index}>
+            <div className="carousel-text-mobile">
+              <div className="carousel-heading-mobile">
+                {item.title}
+              </div>
+              <div className="carousel-subtext-mobile">
+                {item.text}
+              </div>
+            </div>
+            <div className="carousel-img-mobile">
+              <img src={item.img} alt="" />
+            </div>
+          </div>
+        ))}
+      </Carousel>
+
     </div>
   );
 }
