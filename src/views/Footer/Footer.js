b614ad9edf8c6e200817ed6688c11d160a71e945
sachinsharma92 <sachindesigner2018@gmail.com>
2022-12-05 15:59:35 +0530

    FIX: feedback points

--- src/views/Footer/Footer.js
+++ src/views/Footer/Footer.js
@@ -6,27 +6,29 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className={styles.footerWrapper}>
-      <div className={styles.themeText}>
-        <img src={millGroveFooterBold} className={styles.themeTextBold} alt="" />
-        <img src={millGroveFooter} alt="" />
-      </div>
-      <div className={styles.footerLinksWrapper}>
-        <div className={styles.footerLinks}>
-          <Link to="/">Privacy Policy</Link>
-          <Link to="/">Terms & Conditions</Link>
-          <Link to="/">Cookies policy</Link>
+    <div className={styles.footerHomepage}>
+      <div className={styles.footerWrapper}>
+        <div className={styles.themeText}>
+          <img src={millGroveFooterBold} className={styles.themeTextBold} alt="" />
+          <img src={millGroveFooter} alt="" />
         </div>
-        {/* <p>©MILLGROVE, 2022. 28.6063° N, 77.3856° E</p> */}
-        <div className={styles.rightFooter}>
-          <a
-            className="siteUrl"
-            href={"https://haryanarera.gov.in/"}
-            target="#blank"
-          >
-            www.haryanarera.gov.in
-          </a>
-          <span>HRERA2020A0009</span>
+        <div className={styles.footerLinksWrapper}>
+          <div className={styles.footerLinks}>
+            <Link to="/">Privacy Policy</Link>
+            <Link to="/">Terms & Conditions</Link>
+            <Link to="/">Cookies policy</Link>
+          </div>
+          {/* <p>©MILLGROVE, 2022. 28.6063° N, 77.3856° E</p> */}
+          <div className={styles.rightFooter}>
+            <a
+              className="siteUrl"
+              href={"https://haryanarera.gov.in/"}
+              target="#blank"
+            >
+              www.haryanarera.gov.in
+            </a>
+            <span>HRERA2020A0009</span>
+          </div>
         </div>
       </div>
     </div>
